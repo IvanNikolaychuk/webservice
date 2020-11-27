ff540c2010c08a80a0681dd42951e0a23d57ce54
ivan <nikolaychukivan@ya.ru>
2020-11-27 13:08:16 +0100

    improvements

--- src/main/java/com/task/webservice/service/EmailService.java
+++ src/main/java/com/task/webservice/service/EmailService.java
@@ -3,9 +3,14 @@ package com.task.webservice.service;
 import com.task.webservice.model.User;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
+
 @Service
 public class EmailService {
+    private static final Logger LOGGER = Logger.getLogger(EmailService.class.getName());
+
     public void sendEmailUponUserRegistration(User user) {
-        // TODO: implement
+        LOGGER.info("Sending registration email to: " + user.getUsername());
     }
 }
